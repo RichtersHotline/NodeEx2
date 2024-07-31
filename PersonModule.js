f89c12c110114c726c12c55dc6c70b3299bc535d
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-07-31 10:38:49 +0200

    exercise2

---
+++ PersonModule.js
@@ -0,0 +1,13 @@
+function Person (firstName, lastName, emailAddress) {
+this.firstName = firstName
+this. lastName = lastName
+this.emailAddress = emailAddress
+
+
+
+}
+export {
+
+    Person
+
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import {Person} from "./PersonModule.js"
+
+const firstPerson = new Person("Richter", "Berg", "IwasbornintheUSA@gmail.com");
+const secondPerson = new Person("David", "Collins", "Collins@gmail.com");
+const thirdPerson = new Person("Corey", "Zeebra", "TheFans@gmail.com");
+console.log(firstPerson, secondPerson, thirdPerson)
